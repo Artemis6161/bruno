db8cf683b33d1861566153345c3920e015f7c4d1
sangeetha <Artemis6161@github.com>
2024-03-04 22:30:01 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
+// type rafce 
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div className="home">
-        <div className="home-text">
-          <p className="home-text-para">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit magni
-            nesciunt ad consequatur sed, sit officiis voluptatem minus dolor
-            rerum cum accusantium, veniam quidem ipsam, autem optio temporibus
-            ut accusamus.
-          </p>
-        </div>
-      </div>
-      <a
-        name=""
-        id=""
-        className="btn btn-primary"
-        href="#"
-        role="button"
-        onClick={() => {
-          console.log("hello");
-        }}
-      >
-        Button
-      </a>
-    </>
-  );
-
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
