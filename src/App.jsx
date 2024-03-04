5eda5990ac3fe9395b462a2df612ee70a9a5d473
SudhakaranV17 <sudhakaranv17@gmail.com>
2024-03-04 21:58:47 +0530

    added scss and bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="home">
+        <div className="home-text">
+          <p className="home-text-para">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit magni
+            nesciunt ad consequatur sed, sit officiis voluptatem minus dolor
+            rerum cum accusantium, veniam quidem ipsam, autem optio temporibus
+            ut accusamus.
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a
+        name=""
+        id=""
+        className="btn btn-primary"
+        href="#"
+        role="button"
+        onClick={() => {
+          console.log("hello");
+        }}
+      >
+        Button
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
