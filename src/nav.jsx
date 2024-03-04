b96f9b48b5624cecadf147a088689b5688483127
sangeetha <Artemis6161@github.com>
2024-03-04 22:27:41 +0530

    nav left side

--- src/nav.jsx
+++ src/nav.jsx
@@ -4,9 +4,16 @@ import React from 'react'
 
 const nav = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='nav_wrapper'>
+      <div className='left'>
+<ul>
+  <h1>  Menu</h1>
+  <li>Clints</li>
+  <li>Culture</li>
+  <li>&more</li>
+</ul>
+      </div>
+    </section>
   )
 }
 
